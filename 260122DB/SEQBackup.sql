1241da16750b17143ed0539d5b39a78fa39fc93e
권혁준 <gurwns9035@naver.com>
2026-01-22 16:52:37 +0900

    Add files via upload

---
+++ 260122DB/SEQBackup.sql
@@ -0,0 +1,208 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 목요일-1월-22-2026   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence ANSWER_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."ANSWER_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 8 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CART_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."CART_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 155 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CATEGORY_IMAGE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."CATEGORY_IMAGE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 241 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CHAT_MESSAGE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."CHAT_MESSAGE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 113 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence CHAT_ROOM_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."CHAT_ROOM_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 12 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence COUPON_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."COUPON_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 9 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence DELIVERY_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."DELIVERY_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 30 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence FAVORITE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."FAVORITE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence INQUIRY_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."INQUIRY_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 35 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence MAIN_BANNER_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."MAIN_BANNER_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence NOTICE_COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."NOTICE_COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 81 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence NOTICE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."NOTICE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 22 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence ORDER_ITEM_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."ORDER_ITEM_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 103 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence ORDERS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."ORDERS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 83 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PAYMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."PAYMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 68 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence POINT_HISTORY_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."POINT_HISTORY_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PRODUCT_IMAGE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."PRODUCT_IMAGE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 282 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PRODUCT_OPTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."PRODUCT_OPTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 242 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PRODUCT_QNA_ANSWER_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."PRODUCT_QNA_ANSWER_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 81 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PRODUCT_QNA_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."PRODUCT_QNA_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 121 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence PRODUCT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."PRODUCT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 278 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REFUND_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REFUND_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 25 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REGION_SPECIAL_ORDER_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REGION_SPECIAL_ORDER_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REGION_SPECIAL_PRODUCT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REGION_SPECIAL_PRODUCT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 10 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REGION_SPECIAL_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REGION_SPECIAL_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 6 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REVIEW_IMAGE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REVIEW_IMAGE_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 81 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REVIEW_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REVIEW_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 16 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CHAT_MESSAGE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_CHAT_MESSAGE"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 236 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CHAT_MESSAGE_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_CHAT_MESSAGE_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CHAT_ROOM
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_CHAT_ROOM"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 94 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CHAT_ROOM_ID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_CHAT_ROOM_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_COUPON_NO
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_COUPON_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_ISSUE_NO
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_ISSUE_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_NOTI_NO
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_NOTI_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 601 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_SUB_NO
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_SUB_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_WISH_NO
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SEQ_WISH_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 101 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SUBSCRIPTION_ORDER_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SUBSCRIPTION_ORDER_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 4 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SUBSCRIPTION_PLAN_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SUBSCRIPTION_PLAN_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 6 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SUBSCRIPTION_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."SUBSCRIPTION_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 5 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence USER_COUPON_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."USER_COUPON_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 231 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
---
+++ 260122DB/TriggerBackup.sql
@@ -0,0 +1,38 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 목요일-1월-22-2026   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Trigger TRG_CHAT_MESSAGE_BI
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "ADMIN"."TRG_CHAT_MESSAGE_BI" 
+BEFORE INSERT ON ADMIN.P_CHAT_MESSAGE
+FOR EACH ROW
+BEGIN
+  IF :NEW.MESSAGE_ID IS NULL THEN
+    SELECT ADMIN.SEQ_CHAT_MESSAGE_ID.NEXTVAL INTO :NEW.MESSAGE_ID FROM DUAL;
+  END IF;
+
+  IF :NEW.CDATETIME IS NULL THEN :NEW.CDATETIME := SYSDATE; END IF;
+END;
+
+/
+ALTER TRIGGER "ADMIN"."TRG_CHAT_MESSAGE_BI" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TRG_CHAT_ROOM_BI
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "ADMIN"."TRG_CHAT_ROOM_BI" 
+BEFORE INSERT ON ADMIN.P_CHAT_ROOM
+FOR EACH ROW
+BEGIN
+  IF :NEW.ROOM_ID IS NULL THEN
+    SELECT ADMIN.SEQ_CHAT_ROOM_ID.NEXTVAL INTO :NEW.ROOM_ID FROM DUAL;
+  END IF;
+
+  IF :NEW.CDATETIME IS NULL THEN :NEW.CDATETIME := SYSDATE; END IF;
+  :NEW.UDATETIME := SYSDATE;
+END;
+
+/
+ALTER TRIGGER "ADMIN"."TRG_CHAT_ROOM_BI" ENABLE;
